7991c53a7b5719aec5eb9cde19888548798a38c6
hespinoza <hectoraee@gmail.com>
2025-07-16 01:17:44 -0400

    add i18n locales

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    es: {
+      'about.about':'Soy Ingeniero en Informática con más de 8 años de experiencia en desarrollo backend, liderazgo técnico y gestión de equipos en entornos de alta exigencia y plataformas de alta concurrencia.',
+    },
+    en: {
+      'about.about': 'Accueil',
+    },
+  } as const;
